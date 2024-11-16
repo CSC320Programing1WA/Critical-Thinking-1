da720e8a48ded5d0d72a8dfb2b622efa29383d67
CSC320Programing1WA <alrik.eighmy@gmail.com>
2024-11-16 10:33:29 -0800

    Add files via upload

---
+++ CT1.java
@@ -0,0 +1,17 @@
+// Java application to print personal information
+public class PersonalInfo { // Defines the class named PersonalInfo
+    public static void main(String[] args) { // Main method where the program execution begins
+        // Declare variables to store personal information
+        String firstName = "Holly"; // Stores the first name of the person
+        String lastName = "Wood"; // Stores the last name of the person
+        String streetAddress = "123 Punny Street"; // Stores the street address
+        String city = "Joketown"; // Stores the city
+        String zipCode = "42068"; // Stores the zip code
+
+        // Print each piece of information on a separate line
+        // Print all information in a single statement
+        System.out.println(firstName + " " + lastName + "\n" + streetAddress + "\n" + city + "\n" + zipCode);
+        // Combines all variables into a single output with newlines for each line
+    }
+}
+  
